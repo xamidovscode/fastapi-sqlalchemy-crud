0b7dd0abb66d23340ad67fb94ff5785fceaffc2f
xamidovscode <xamidovcoder@gmail.com>
2025-08-06 18:55:31 +0500

    added a .env file

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Boilerplate"
     API_V1_STR: str = "/api/v1"
-    DATABASE_URL: str = "postgresql+asyncpg://fastapi_db:fastapi_db@locahost:5432/fastapi_db"
+    DATABASE_URL: str = "postgresql+asyncpg://db_user:password@host:port/db_name"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
